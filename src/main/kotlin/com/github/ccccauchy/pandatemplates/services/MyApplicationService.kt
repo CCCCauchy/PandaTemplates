679dec4f11f6f3deaa446c24a6d9e31f52bfa7c7
GitHub Action <action@github.com>
2021-04-27 05:37:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/ccccauchy/pandatemplates/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ccccauchy/pandatemplates/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ccccauchy.pandatemplates.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ccccauchy.pandatemplates.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ccccauchy/pandatemplates/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ccccauchy/pandatemplates/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ccccauchy.pandatemplates.services
 
+import com.github.ccccauchy.pandatemplates.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
